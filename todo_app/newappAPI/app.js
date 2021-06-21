ee8467eb8bd41ad8ee198b59dcbe93e1dcbe896e
Puneet311 <puneetpandey311@gmail.com>
2021-06-21 17:39:08 +0530

    changes for deploy

--- todo_app/newappAPI/app.js
+++ todo_app/newappAPI/app.js
@@ -4,11 +4,16 @@ const bodyparser=require("body-parser");
 const cors=require('cors')
 const app=express();
 
+app.use(express.static(root_-__dirname+'/dist'));
 app.use(cors())
 app.use(bodyparser.json())
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+})
 app.use('/myapp',router);
 
+const port=process.env.PORT ||3000;
 
-app.listen(3000);
+app.listen(port);
 console.log("server is listening to port 3000");
